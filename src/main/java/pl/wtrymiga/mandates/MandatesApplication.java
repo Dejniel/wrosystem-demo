55f55d736c8c1f8f519c42552c19eab3855c3fc3
Daniel <github@inajiffy.eu>
2024-12-09 20:43:27 +0100

    init

---
+++ src/main/java/pl/wtrymiga/mandates/MandatesApplication.java
@@ -0,0 +1,13 @@
+package pl.wtrymiga.mandates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wtrymiga/mandates/MandatesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wtrymiga.mandates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
