14175088910f219264fe11aae9c396722c89f63d
Daniel <github@inajiffy.eu>
2024-12-13 01:16:44 +0100

    form js refactor

--- src/main/resources/static/js/form.js
+++ src/main/resources/static/js/form.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-	
+	// Walidacja danych
 	document.getElementById("mandateForm").onsubmit = async (e) => {
 		e.preventDefault();
 		if (!e.target.checkValidity()) {
@@ -24,43 +24,37 @@ document.addEventListener("DOMContentLoaded", function(){
 		}
 	};
 
-	const employeeType = document.getElementById("employeeType");
-	const companySelect = document.getElementById("company");
-	const employeeSelect = document.getElementById("employee");
-	const reasonSelect = document.getElementById("reason");
-	
-	reasonSelect.onchange = () => document.getElementById("other-reason").classList.toggle("d-none", reasonSelect.value !== "OTHER");
-	reasonSelect.onchange();
-	
+
+	// Dynamiczne wartości
 	const fetchOptions = async (url, select, defaultText, mapFn) => {
 		const data = await fetch(url).then(res => res.json());
 		select.innerHTML = `<option value="">${defaultText}</option>` + data.map(mapFn).join('');
 	};
-
-	employeeType.onchange =  async() => {
-		document.getElementById("physical-fields").classList.toggle("d-none", employeeType.value !== "PHYSICAL");
-		document.getElementById("office-fields").classList.toggle("d-none", employeeType.value !== "OFFICE");
+	const employeeTypeSelect = document.getElementById("employeeType");
+	const companySelect = document.getElementById("company");
+	employeeTypeSelect.onchange =  async() => {
+		document.getElementById("physical-fields").classList.toggle("d-none", employeeTypeSelect.value !== "PHYSICAL");
+		document.getElementById("office-fields").classList.toggle("d-none", employeeTypeSelect.value !== "OFFICE");
 		document.querySelectorAll("#physical-fields input").forEach(input => {
-			input.required = employeeType.value === "PHYSICAL";  
+			input.required = employeeTypeSelect.value === "PHYSICAL";  
 		});
 		document.querySelectorAll("#office-fields input").forEach(input => {
-			input.required = employeeType.value === "OFFICE";  
+			input.required = employeeTypeSelect.value === "OFFICE";  
 		});
-		if (employeeType.value === "PHYSICAL")
+		if (employeeTypeSelect.value === "PHYSICAL")
 			await fetchOptions('/api/get/companies', companySelect, "Wybierz firmę...", c => `<option value="${c.id}">${c.name}</option>`);
-		if(employeeType.value === "OFFICE")
+		if(employeeTypeSelect.value === "OFFICE")
 			companySelect.value = '';
-	};		
-	companySelect.onchange = () => fetchOptions(
+	};	
+	const employeeSelect = document.getElementById("employee");	
+	companySelect.onchange = () => companySelect.value && fetchOptions(
 		`/api/get/employees?companyId=${companySelect.value}`, 
 		employeeSelect, 
 		"Wybierz pracownika...", 
 		e => `<option value="${e.id}">${e.firstName} ${e.lastName}</option>`
 	);
-
-
 	(async () => {
-		await employeeType.onchange();
+		await employeeTypeSelect.onchange();
 		if(companySelect.dataset.selected){
 			companySelect.value = companySelect.dataset.selected;
 			await companySelect.onchange();
@@ -68,30 +62,39 @@ document.addEventListener("DOMContentLoaded", function(){
 		}
 	})();
 	
+	const reasonSelect = document.getElementById("reason");
+	reasonSelect.onchange = () => document.getElementById("other-reason").classList.toggle("d-none", reasonSelect.value !== "OTHER");
+	reasonSelect.onchange();
 	
-
-	const fileToUrl = () => URL.createObjectURL(new Blob([new Uint8Array(document.getElementById('attachment').value.split(',').map(Number).map(n => (n + 256) % 256))], { type: 'application/pdf' }));
+	
+	// Obsługa załączników
+	const attachmentInput = document.getElementById("attachment");
+	const fileMenuContainer = document.getElementById('fileMenu');
+	const fileToUrl = () => URL.createObjectURL(new Blob([new Uint8Array(attachmentInput.value.split(',').map(Number).map(n => (n + 256) % 256))], { type: 'application/pdf' }));
+	
+	
 	document.getElementById("fileUpload").addEventListener("change", async e => {
 		if (e.target.files[0]) {
-			document.getElementById("attachment").value = Array.from(new Uint8Array(await e.target.files[0].arrayBuffer())).map(b => b > 127 ? b - 256 : b).join(',');
-			document.getElementById('fileMenu').classList.remove('d-none');
+			attachmentInput.value = Array.from(new Uint8Array(await e.target.files[0].arrayBuffer())).map(b => b > 127 ? b - 256 : b).join(',');
+			fileMenuContainer.classList.remove('d-none');
 		}
 	});
-	const previewFile = () => {
+	document.getElementById("filePreview").addEventListener("click", async () => {
 		window.open(fileToUrl(), '_blank');
 		console.log(fileToUrl());
-	};
-	const downloadFile = () => {
+	});
+	document.getElementById("fileDownload").addEventListener("click", async () => {
 		const link = document.createElement('a');
 		link.href = fileToUrl();
 		link.download = document.getElementById("signature").value + "_attachment.pdf";
 		link.click();
 		URL.revokeObjectURL(link);
-	};
-	const removeFile = () => { 
-		document.getElementById('attachment').value = ''; 
-		document.getElementById('fileMenu').classList.add('d-none');
-	};
-	if(document.getElementById('attachment').value)
-		document.getElementById('fileMenu').classList.remove('d-none');
+	});
+	document.getElementById("fileDelete").addEventListener("click", async () => { 
+		attachmentInput.value = ''; 
+		fileMenuContainer.classList.add('d-none');
+	});
+	
+	if(attachmentInput.value)
+		fileMenuContainer.classList.remove('d-none');
 });
